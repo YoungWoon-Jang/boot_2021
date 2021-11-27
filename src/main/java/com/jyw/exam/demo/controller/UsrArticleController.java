cc94a0c5376cb7df66a6fcf6aff308ae0ae55b4f
YoungWoon-Jang <duddns6133@naver.com>
2021-11-27 18:19:01 +0900

    게시물 수정

--- src/main/java/com/jyw/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jyw/exam/demo/controller/UsrArticleController.java
@@ -60,6 +60,13 @@ public class UsrArticleController {
 		
 		articles.remove(article);
 	}
+	
+	private void modifyArtilce(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+	}
 	// 서비스 메소드 끝
 
 	// 액션 메소드 시작
@@ -91,5 +98,20 @@ public class UsrArticleController {
 
 		return id + "번 게시물을 삭제했습니다.";
 	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArtilce(id, title, body);
+
+		return id + "번 게시물을 수정했습니다.";
+	}
 	// 액션 메소드 끝
+
 }
